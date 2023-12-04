3662ee35c71458e1c9bdb054919b9f7bcfa83e4e
Daniël Botha <daniel@polymorph.co.za>
2023-12-04 16:59:35 +0200

    add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Landing from './components/Landing';
 import Nav from './components/Nav';
-import saveTheDate from '../public/save-the-date.jpeg';
 
 function App() {
   return (
     <div className="h-screen bg-[url('/save-the-date.jpeg')] bg-no-repeat bg-cover  bg-center relative w-full">
       <div className="h-full w-full backdrop-blur-2xl p-2 sm:p-8 flex items-center">
         <img
-          src={saveTheDate}
+          src="/save-the-date.jpeg?url"
           className="mx-auto backdrop-blur-md shadow-xl  max-h-full w-full h-max"
           style={{
             aspectRatio: '9 / 16',
