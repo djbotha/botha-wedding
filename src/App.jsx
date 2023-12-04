a6db666bb2a87221dc92d1dc3201bd3298a41d2b
Daniël Botha <daniel@polymorph.co.za>
2023-12-04 17:13:49 +0200

    tweak vertical alignment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import saveTheDate from '../public/save-the-date.jpeg';
 function App() {
   return (
     <div className="h-screen bg-[url('/save-the-date.jpeg')] bg-no-repeat bg-cover  bg-center relative w-full">
-      <div className="h-full w-full backdrop-blur-2xl p-2 sm:p-8 flex items-center">
+      <div className="h-full w-full backdrop-blur-2xl p-2 sm:p-8 flex xs:items-center">
         <img
           src={saveTheDate}
           className="mx-auto backdrop-blur-md shadow-xl  max-h-full w-full h-max"
