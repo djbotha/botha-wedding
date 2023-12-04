76855931048f12c9c8e427f19f0e0cff0197b126
Daniël Botha <daniel@polymorph.co.za>
2023-12-04 08:47:34 +0200

    Add STD

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="h-screen bg-black relative">
-      <Nav />
-      <Landing />;
+    <div className="h-screen bg-[url('/save-the-date.jpeg')] bg-no-repeat bg-cover  bg-center relative w-full">
+      <div className="h-full w-full backdrop-blur-2xl p-2 sm:p-8 flex">
+        <img
+          src="/save-the-date.jpeg"
+          className="mx-auto backdrop-blur-md shadow-xl aspect-auto  max-h-full"
+          style={{
+            aspectRatio: '16/9',
+          }}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hero() {
   return (
-    <div className="bg-[url('/hero.jpeg')] w-full bg-no-repeat bg-cover bg-center pt-64 pb-8">
+    <div className="bg-[url('/save-the-date.jpeg')] w-full bg-no-repeat bg-cover bg-center pt-64 pb-8">
       <h1 className="text-center text-8xl">Daniël & Nadia</h1>
       <h2 className="text-center text-lg">22 Jun 2024</h2>
       <h2 className="text-center">Belair Pavilion, Paarl</h2>
