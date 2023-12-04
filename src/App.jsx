89e51d8416186334c1affba670e3a2a709d281d0
Daniël Botha <daniel@polymorph.co.za>
2023-12-04 08:58:53 +0200

    fix width styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Nav from './components/Nav';
 function App() {
   return (
     <div className="h-screen bg-[url('/save-the-date.jpeg')] bg-no-repeat bg-cover  bg-center relative w-full">
-      <div className="h-full w-full backdrop-blur-2xl p-2 sm:p-8 flex">
+      <div className="h-full w-full backdrop-blur-2xl p-2 sm:p-8 flex items-center">
         <img
           src="/save-the-date.jpeg"
-          className="mx-auto backdrop-blur-md shadow-xl aspect-auto  max-h-full"
+          className="mx-auto backdrop-blur-md shadow-xl  max-h-full w-full h-max"
           style={{
-            aspectRatio: '16/9',
+            aspectRatio: '9 / 16',
+            width: 510,
           }}
         />
       </div>
