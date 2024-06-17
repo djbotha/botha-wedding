18eaecfd61803420939678fe26859978f5d24d12
Daniël Botha <daniel@polymorph.co.za>
2024-06-17 13:20:10 +0200

    Update on the day schedule

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -3,15 +3,57 @@ import React from 'react';
 export function Schedule() {
   return (
     <div className="flex justify-center p-8">
-      <div className="justify-center lg:w-1/2 lg:p-0">
+      <div className="justify-center  lg:w-1/2 lg:p-0">
+        <p className="mb-4">
+          Here are the details for the schedule, accommodation, and timings for the day. If you have any questions, please don't hesitate to call us!
+        </p>
         <div className="mb-4">
-          <b>Date & Time</b>
+          <b class="text-lg">Date &amp; Time</b>
           <ul className="list-inside list-disc">
-            <li>22 June 2024</li>
-            <li>14:00 for 14:30</li>
+            <li>
+              <b>Date:</b> 22 June 2024
+            </li>
+            <li>
+              <b>Time:</b> 14:00 for 14:30
+            </li>
           </ul>
         </div>
-        <p className="mb-4">More details coming soon...</p>
+        <div className="mb-4">
+          <b class="text-lg">Accommodation</b>
+          <ul className="list-inside list-disc">
+            <li>
+              <b>Check-In Time:</b> Between 14:00 and 18:00 (We recommend checking in before the ceremony).
+            </li>
+            <li>
+              <b>Location:</b> On-site, a quick 5-minute walk from the ceremony area.
+            </li>
+            <li>
+              <b>Complimentary Breakfast:</b> At the Goatshed Restaurant at Fairview from 09:00 to 11:00 on Sunday.
+            </li>
+            <li>
+              <b>Note:</b> Please keep your room keys with you at all times.
+            </li>
+          </ul>
+        </div>
+
+        <div className="mb-4">
+          <b class="text-lg">Schedule</b>
+          <ul className="list-inside list-disc">
+            <li>
+              <b>Guests Arrival:</b> 14:00
+            </li>
+            <li>
+              <b>Ceremony:</b> 15:00
+            </li>
+            <li>
+              <b>Pre-drinks:</b> 15:45 - 17:30
+            </li>
+            <li>
+              <b>Reception:</b> 17:30 - 23:00
+            </li>
+          </ul>
+        </div>
+        <p>Thank you for your attention to these important details. We look forward to celebrating with you on our special day!</p>
       </div>
     </div>
   );
